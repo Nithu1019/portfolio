254b8d46e502624f092f99f9c48be8b69adfe512
Nithu1019 <k.nithusan.sio@gmail.com>
2022-06-07 09:58:51 +0200

    js for nav

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 //get section in main
-const Sections = document.querySelectorAll("main section");
+const Sections = document.querySelectorAll("section");
 
 for (let index = 0; index < Sections.length; index++) {
   Sections.forEach((element) => {
@@ -9,3 +9,24 @@ for (let index = 0; index < Sections.length; index++) {
     index++;
   });
 }
+
+
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav .container ul li");
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+});
